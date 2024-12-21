c1a75484a9b10bfb1bbd72ce6cf0cbf47c69af87
ziiixoi <zigooda3@gmail.com>
2024-12-22 01:11:18 +0900

    Update built files for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,22 @@
 export default {
-	resolve: {
-		alias: {
-		  three: 'node_modules/three/build/three.module.js',
-		},
-	  },
-	root: 'src/',
-	publicDir: '../public/',
-	base: './',
-	build:
-	{
-		outDir: '../dist',
-		emptyOutDir: true,
-		sourcemap: true
-	}
-}
+    resolve: {
+        alias: {
+            three: 'node_modules/three/build/three.module.js',
+            'three/addons': 'node_modules/three/examples/jsm', // 추가 모듈 경로
+        },
+    },
+    root: 'src/',
+    publicDir: '../public/',
+    base: '/MyWebsite/', // GitHub Pages에 맞는 경로 설정
+    build: {
+        outDir: '../docs', // docs 폴더에 빌드 결과 저장
+        emptyOutDir: true,
+        sourcemap: true,
+    },
+    optimizeDeps: {
+        include: [
+            'three',
+            'three/examples/jsm/controls/OrbitControls.js',
+        ],
+    },
+};
